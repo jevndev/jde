f3914b14ff923e05384d4ff678dde5922a80d052
Jonathan Evans <JonathanEvansDev@gmail.com>
2025-12-19 16:57:26 -0600

    Add init to cumulative sum

--- src/jde/itertools.hpp
+++ src/jde/itertools.hpp
@@ -2,98 +2,128 @@
 
 #include <ranges>
 
-namespace jde
-{
+namespace jde {
 
-template <typename V>
-  requires std::ranges::view<V>
-struct cumulative_sum_view : public std::ranges::view_interface<cumulative_sum_view<V>>
-{
+namespace detail {
+
+template <typename V, typename AccumulatorType>
+class cumulative_sum_iter {
 public:
-  using T = std::ranges::range_value_t<V>;
-
-  template <bool Const>
-  class Iter
-  {
-  public:
-    using UnderlyingIteratorT = std::ranges::iterator_t<std::conditional_t<Const, const V, V>>;
-    using difference_type = typename UnderlyingIteratorT::difference_type;
+    using T = std::ranges::range_value_t<V>;
+    using UnderlyingIteratorT = std::ranges::iterator_t<V>;
+    using difference_type = std::iter_difference_t<UnderlyingIteratorT>;
     using value_type = T;
 
-    constexpr explicit Iter(UnderlyingIteratorT it, UnderlyingIteratorT end) noexcept
-        : m_curr { it }, m_end { end }
-    {
-      if (m_curr != m_end)
-      {
-        m_accumulator = *m_curr;
-      }
+    constexpr explicit cumulative_sum_iter(UnderlyingIteratorT it, UnderlyingIteratorT end,
+                                           AccumulatorType init) noexcept
+        : m_curr{it}, m_end{end}, m_accumulator{std::move(init)} {
+        if (m_curr != m_end) {
+            m_accumulator += *m_curr;
+        }
     }
 
-    constexpr Iter(const Iter&) = default;
-    constexpr Iter& operator=(const Iter&) = default;
-    constexpr Iter& operator=(Iter&&) = default;
-    constexpr Iter(Iter&& other) noexcept = default;
-    constexpr ~Iter() = default;
+    constexpr cumulative_sum_iter(const cumulative_sum_iter &) = default;
+    constexpr cumulative_sum_iter &operator=(const cumulative_sum_iter &) = default;
+    constexpr cumulative_sum_iter &operator=(cumulative_sum_iter &&) = default;
+    constexpr cumulative_sum_iter(cumulative_sum_iter &&other) noexcept = default;
+    constexpr ~cumulative_sum_iter() = default;
 
-    constexpr Iter& operator++() {
-      if (m_curr != m_end)
-      {
-        m_curr++;
-        if (m_curr != m_end) m_accumulator += *m_curr;
-      }
-      return *this;
-    }
-
-    constexpr Iter operator++(int) {
-      if (m_curr != m_end)
-      {
-        auto tmp = *this;
-        ++(*this);
-        return tmp;
-      } else {
+    constexpr cumulative_sum_iter &operator++() {
+        if (m_curr != m_end) {
+            m_curr++;
+            if (m_curr != m_end)
+                m_accumulator += *m_curr;
+        }
         return *this;
-      }
     }
 
-    [[nodiscard]] constexpr bool operator==(const Iter& rhs) const {
-      return m_curr == rhs.m_curr && m_end == rhs.m_end;
+    constexpr cumulative_sum_iter operator++(int) {
+        if (m_curr != m_end) {
+            auto tmp = *this;
+            ++(*this);
+            return tmp;
+        } else {
+            return *this;
+        }
     }
 
-    [[nodiscard]] constexpr bool operator==([[maybe_unused]] const std::default_sentinel_t sentinel) const {
-      return m_curr == m_end;
+    [[nodiscard]] constexpr bool operator==(const cumulative_sum_iter &rhs) const {
+        return m_curr == rhs.m_curr && m_end == rhs.m_end;
     }
 
-    [[nodiscard]] constexpr T operator*() const {return m_accumulator;}
+    [[nodiscard]] constexpr bool
+    operator==([[maybe_unused]] const std::default_sentinel_t sentinel) const {
+        return m_curr == m_end;
+    }
 
-  private:
+    [[nodiscard]] constexpr T operator*() const { return m_accumulator; }
+
+private:
     UnderlyingIteratorT m_curr;
     UnderlyingIteratorT m_end;
     T m_accumulator;
-  };
+};
 
-  constexpr explicit cumulative_sum_view(V&& view) noexcept
-      : m_range { std::move(view) }
-  {
-  }
+template <typename V, typename AccumulatorType>
+class cumulative_sum_view
+    : public std::ranges::view_interface<cumulative_sum_view<V, AccumulatorType>> {
+public:
+    using T = std::ranges::range_value_t<V>;
 
-  [[nodiscard]] constexpr auto begin() {return Iter<false>{std::ranges::begin(m_range), std::ranges::end(m_range)};}
-  [[nodiscard]] constexpr auto begin() const {return Iter<true>{std::ranges::begin(m_range), std::ranges::end(m_range)};}
-  [[nodiscard]] constexpr auto end() const {return std::default_sentinel;}
+    constexpr explicit cumulative_sum_view(V view, AccumulatorType init) noexcept
+        : m_range{std::move(view)}, m_init{std::move(init)} {}
+
+    [[nodiscard]] constexpr auto begin() {
+        return cumulative_sum_iter<V, AccumulatorType>{std::ranges::begin(m_range),
+                                                       std::ranges::end(m_range),
+                                                       AccumulatorType{m_init}};
+    }
+
+    [[nodiscard]] constexpr auto begin() const {
+        return cumulative_sum_iter<const V, AccumulatorType>{std::ranges::begin(m_range),
+                                                             std::ranges::end(m_range),
+                                                             AccumulatorType{m_init}};
+    }
+
+    [[nodiscard]] constexpr auto end() const { return std::default_sentinel; }
 
 private:
-  V m_range;
+    V m_range;
+    AccumulatorType m_init;
 };
 
-struct cumulative_sum_t : public std::ranges::range_adaptor_closure<cumulative_sum_t>
-{
-  template <typename R>
-    requires std::ranges::viewable_range<R>
-  constexpr auto operator()(const R& r) const
-  {
-    return cumulative_sum_view { std::views::all(r) };
-  }
+} // namespace detail
+
+template <typename AccumulatorType = void>
+class cumulative_sum : public std::ranges::range_adaptor_closure<cumulative_sum<AccumulatorType>> {
+public:
+    constexpr explicit cumulative_sum() noexcept : m_init{} {}
+
+    constexpr explicit cumulative_sum(const AccumulatorType &init) noexcept : m_init{init} {}
+
+    template <typename R>
+        requires std::ranges::viewable_range<R> &&
+                 std::convertible_to<std::ranges::range_value_t<R>, AccumulatorType>
+    constexpr auto operator()(const R &r) const {
+        return detail::cumulative_sum_view{std::views::all(r), AccumulatorType{m_init}};
+    }
+
+private:
+    AccumulatorType m_init;
 };
 
-static constexpr inline auto cumulative_sum = cumulative_sum_t{};
+template <>
+class cumulative_sum<void> : public std::ranges::range_adaptor_closure<cumulative_sum<void>> {
+public:
+    constexpr explicit cumulative_sum() noexcept = default;
 
-}
+    template <std::ranges::viewable_range R>
+    constexpr auto operator()(const R &r) const {
+        return detail::cumulative_sum_view{std::views::all(r), std::ranges::range_value_t<R>{}};
+    }
+};
+
+template <typename T>
+cumulative_sum(T) -> cumulative_sum<T>;
+
+} // namespace jde
