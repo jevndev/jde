9c8d44f9e898d7b4d0e8e098c57a73ecbd7f95b6
Jonathan Evans <JonathanEvansDev@gmail.com>
2026-02-07 11:26:14 -0600

    Add SpecializationOf

--- src/jde/concepts.hpp
+++ src/jde/concepts.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
 #include <type_traits>
+
+#include "metaprogramming.hpp"
+
 namespace jde {
 
 template <typename T>
 concept Pointer = requires(T t) { *t; } && std::is_convertible_v<T, void *>;
+
+template<typename T, template<typename...> typename U>
+concept SpecializationOf = detail::is_specialization_of<T, U>::value;
 
 } // namespace jde
--- src/jde/metaprogramming.hpp
+++ src/jde/metaprogramming.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <tuple> // IWYU pragma: keep
+#include <type_traits>
 
 namespace jde {
 
@@ -31,6 +32,12 @@ struct GetReturnType<R (T::*)(Args...) const> {
     using Type = R;
 };
 
+template<typename T, template<typename...> typename Primary>
+struct is_specialization_of : std::false_type {};
+
+template<template<typename...> typename Primary, typename... Args>
+struct is_specialization_of<Primary<Args...>, Primary> : std::true_type {};
+
 } // namespace detail
 
 namespace MemberFunction {
@@ -54,5 +61,8 @@ concept is_nonempty_tuple_like = requires(T t) {
 
 template <typename T>
 concept is_tuple_like = is_empty_tuple_like<T> || is_nonempty_tuple_like<T>;
+
+template<typename T, template<typename...> typename Primary>
+inline constexpr bool is_specialization_of_v = detail::is_specialization_of<T, Primary>::value;
 
 } // namespace jde
