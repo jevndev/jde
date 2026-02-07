c2f0ceae391533058fa2ffa9fa3b5f20e31d5f99
Jonathan Evans <JonathanEvansDev@gmail.com>
2026-02-07 11:22:26 -0600

    Add Default to variant helpers

--- src/jde/variant.hpp
+++ src/jde/variant.hpp
@@ -13,6 +13,20 @@ struct overloaded : public T... {
 template <typename... T>
 overloaded(T...) -> overloaded<T...>;
 
+template <typename Fn>
+requires std::is_invocable_v<Fn>
+struct Default
+{
+    explicit Default(Fn&& fn): m_fn{fn} {}
+
+    auto operator()([[maybe_unused]] const auto&){
+        return m_fn();
+    }
+
+private:
+    Fn m_fn;
+};
+
 constexpr auto visit(auto &&variant, auto &&fns...) {
     return std::visit(overloaded(std::forward<decltype(fns)>(fns)),
                       std::forward<decltype(variant)>(variant));
